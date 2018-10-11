3a2415295f6261ef689a4ba63e2a7d0479b293c1
Kundan Thakur <kundanthakurcs@gmail.com>
2018-10-11 13:08:53 +0530

    addedallfiles

---
+++ src/main/java/childpom/childpom/App.java
@@ -0,0 +1,13 @@
+package childpom.childpom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
